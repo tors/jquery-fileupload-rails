20103d38951dc1515dc3b0f03ae64ac5b17d183d
Nadeem Bitar <nadeem@gmail.com>
2012-03-24 18:23:58 -0700

    Added missing dependencies

--- vendor/assets/javascripts/jquery-fileupload/index.js
+++ vendor/assets/javascripts/jquery-fileupload/index.js
@@ -1,4 +1,7 @@
 //=require jquery-fileupload/vendor/jquery.ui.widget
+//=require jquery-fileupload/vendor/load-image
+//=require jquery-fileupload/vendor/canvas-to-blob
+//=require jquery-fileupload/vendor/tmpl
 //=require jquery-fileupload/jquery.iframe-transport
 //=require jquery-fileupload/jquery.fileupload
 //=require jquery-fileupload/jquery.fileupload-ip
