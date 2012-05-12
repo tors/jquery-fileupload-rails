a68ad01c2f85058e06f0ccbb33edb2e265bfbd71
Patrick Schmitz <p.schmitz@gmail.com>
2012-05-12 14:00:30 +0200

    update jquery-File-Upload to v6.7.0

--- vendor/assets/javascripts/jquery-fileupload/index.js
+++ vendor/assets/javascripts/jquery-fileupload/index.js
@@ -4,6 +4,6 @@
 //=require jquery-fileupload/vendor/tmpl
 //=require jquery-fileupload/jquery.iframe-transport
 //=require jquery-fileupload/jquery.fileupload
-//=require jquery-fileupload/jquery.fileupload-ip
+//=require jquery-fileupload/jquery.fileupload-fp
 //=require jquery-fileupload/jquery.fileupload-ui
 //=require jquery-fileupload/locale
